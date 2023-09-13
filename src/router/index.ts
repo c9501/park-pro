1ae1d9ba38214baeb8b103ec5b968c2d19bfcfc7
c9501 <549105847@qq.com>
2023-09-13 15:25:07 +0800

    第一次提交

---
+++ src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import BigScreen from '../views/BigScreen/index.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      redirect: '/big-screen'
+    },
+    {
+      path: '/big-screen',
+      name: 'big-screen',
+      component: BigScreen
+    }
+  ]
+})
+
+export default router
